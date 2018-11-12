b3c20a71b6a921e5080ce81f0ffc22a80a692ce0
gabrielconte14 <gabrielconte14@gmail.com>
2018-11-12 19:06:32 -0200

    Updated Events entity

--- EventsApp/EventsApp/Model/Entity/Event.swift
+++ EventsApp/EventsApp/Model/Entity/Event.swift
@@ -11,5 +11,11 @@ import Foundation
 struct Event: Decodable{
     
     let id: String?
+    let title: String?
+    let price: Float?
+    let latitude: String?
+    let longitude: String?
+    let image: String?
+    let description: String?
     
 }
