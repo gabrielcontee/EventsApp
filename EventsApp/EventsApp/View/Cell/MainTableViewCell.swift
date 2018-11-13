1b0f696530c6e707b980d01585154c17fa95026a
gabrielconte14 <gabrielconte14@gmail.com>
2018-11-12 22:15:18 -0200

    Created Event List Xib and Cell Xib

---
+++ EventsApp/EventsApp/View/Cell/MainTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MainTableViewCell.swift
+//  EventsApp
+//
+//  Created by Gabriel Conte on 12/11/18.
+//  Copyright © 2018 Gabriel Conte. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ EventsApp/EventsApp/View/Controller/MainTableViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainTableViewController.swift
+//  EventsApp
+//
+//  Created by Gabriel Conte on 12/11/18.
+//  Copyright © 2018 Gabriel Conte. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
